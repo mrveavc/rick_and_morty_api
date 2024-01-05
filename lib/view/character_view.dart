07b77a92e942a73207d0adb0c752d10206eb365b
mrveavc <mrveavc12@gmail.com>
2024-01-05 12:03:39 +0300

    edit

--- lib/view/character_view.dart
+++ lib/view/character_view.dart
@@ -17,93 +17,88 @@ class CharacterView extends StatelessWidget {
             itemCount: viewModel.listCharacter.length,
             itemBuilder: (context, index) {
               Character character = viewModel.listCharacter[index];
-              return Card(
-                color: const Color.fromARGB(255, 82, 81, 81),
-                child: Column(
-                  children: [
-                    Row(
-                      children: [
-                        Expanded(
-                            flex: 2, child: Image.network(character.image)),
-                        Expanded(
-                          flex: 3,
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  character.name,
-                                  style: const TextStyle(
-                                      color: Colors.white, fontSize: 20),
-                                ),
-                                Row(
-                                  children: [
-                                    if (character.status == 'Dead')
-                                      const Icon(Icons.radio_button_checked,
-                                          color: Colors.red, size: 10),
-                                    if (character.status == 'Alive')
-                                      const Icon(Icons.radio_button_checked,
-                                          color: Colors.green, size: 10),
-                                    Text(
-                                      character.status,
-                                      style: const TextStyle(
-                                          color: Colors.white, fontSize: 16),
-                                    ),
-                                    const Text(
-                                      " - ",
-                                      style: TextStyle(
-                                          color: Colors.white, fontSize: 16),
-                                    ),
-                                    Text(
-                                      character.species,
-                                      style: const TextStyle(
-                                          color: Colors.white, fontSize: 16),
-                                    ),
-                                  ],
-                                ),
-                                const SizedBox(
-                                  height: 10,
-                                ),
-                                const Text(
-                                  "Last know location:",
-                                  style: TextStyle(
-                                      color: Colors.grey, fontSize: 15),
-                                ),
-                                Text(
-                                  character.location,
-                                  style: const TextStyle(
-                                      color: Colors.white, fontSize: 15),
-                                ),
-                                const SizedBox(
-                                  height: 10,
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ],
-                    )
-
-                    // Image.network(character.image),
-                    // Text(character.name),
-                  ],
-                ),
-              );
-              // return ListTile(
-              //   title: Text(character.name),
-              //   subtitle: Text(character.status),
-              //   leading: Column(
-              //     children: [
-              //       CircleAvatar(
-              //         backgroundImage: NetworkImage(character.image),
-              //       ),
-              //       Text(character.species)
-              //     ],
-              //   ),
-              // );
+              return _buildCard(character);
             },
           ),
         ));
+  }
+
+  Widget _buildCard(Character character) {
+    return Card(
+      color: const Color.fromARGB(255, 82, 81, 81),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(flex: 2, child: Image.network(character.image)),
+              Expanded(
+                flex: 3,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        character.name,
+                        style:
+                            const TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                      Row(
+                        children: [
+                          if (character.status == 'Dead')
+                            const Icon(Icons.radio_button_checked,
+                                color: Colors.red, size: 10),
+                          if (character.status == 'Alive')
+                            const Icon(Icons.radio_button_checked,
+                                color: Colors.green, size: 10),
+                          if (character.status == 'unknown')
+                            const Icon(Icons.radio_button_checked,
+                                color: Colors.yellow, size: 10),
+                          const SizedBox(
+                            width: 5,
+                          ),
+                          Text(
+                            character.status,
+                            style: const TextStyle(
+                                color: Colors.white, fontSize: 16),
+                          ),
+                          const Text(
+                            " - ",
+                            style: TextStyle(color: Colors.white, fontSize: 16),
+                          ),
+                          Text(
+                            character.species,
+                            style: const TextStyle(
+                                color: Colors.white, fontSize: 16),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      const Text(
+                        "Last know location:",
+                        style: TextStyle(color: Colors.grey, fontSize: 15),
+                      ),
+                      Text(
+                        character.location,
+                        style:
+                            const TextStyle(color: Colors.white, fontSize: 15),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          )
+
+          // Image.network(character.image),
+          // Text(character.name),
+        ],
+      ),
+    );
   }
 }
